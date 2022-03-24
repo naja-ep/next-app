f7120eccf2d9b51c49bdafd0ecf2fb26b5dbd681
naja-ep <naja@epart.com>
2022-03-25 08:35:41 +0900

    added header, footer wrapper

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,15 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="footer_info">
-        서울시 송파구 오금로 311 (오금동45-1) (주)일룸 ·대표이사 강성문 개인정보보호 책임자 : 강성문 사업자등록번호 : 215-86-93600·통신판매업신고 : 2009-서울송파-0069호·부가통신사업신고필증 : 021129
-        호스팅 서비스사업자 · (주)일룸 webmaster@iloom.com·Copyright (c) 2018 iloom lnc. All rights reserved
+      <div className="wrapper">
+        <div className="footer_info">
+          <p>서울시 송파구 오금로 311 (오금동45-1) (주)일룸 </p>
+          <p>·대표이사 강성문 개인정보보호 책임자 : 강성문 사업자등록번호 : 215-86-93600</p>
+          <p>·통신판매업신고 : 2009-서울송파-0069호</p>
+          <p>·부가통신사업신고필증 : 021129 호스팅 서비스사업자 </p>
+          <p>·(주)일룸 webmaster@iloom.com</p>
+          <p>·Copyright (c) 2018 iloom lnc. All rights reserved</p>
+        </div>
       </div>
     </footer>
   );
--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,46 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header>
-      <h1>
-        <img src="/images/logo.png" alt="일룸" />
-      </h1>
-      <nav className="gnb">
-        <ul className="depth_1">
-          <li>
-            <a href="#">Menu 1</a>
-          </li>
-          <li>
-            <a href="#">Menu 2</a>
-          </li>
-          <li>
-            <a href="#">Menu 3</a>
-          </li>
-          <li>
-            <a href="#">Menu 4</a>
-          </li>
-          <li>
-            <a href="#">Menu 5</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="header_wrap clearfix">
+        <h1>
+          <Link href="/">
+            <a>
+              <img src="/images/logo.png" alt="일룸" />
+            </a>
+          </Link>
+        </h1>
+        <nav className="gnb">
+          <ul className="depth_1">
+            <li>
+              <Link href="/pages/src/bedroom">
+                <a>침실</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <a>거실</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <a>주방</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <a>키즈룸</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="#">
+                <a>학생방</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
